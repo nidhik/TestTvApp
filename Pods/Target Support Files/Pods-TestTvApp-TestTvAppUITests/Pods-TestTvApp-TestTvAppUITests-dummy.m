91fbb0cf0a516277ece60aa52b32f3d81f6671f0
Nidhi Kulkarni <nkulkarni@mux.com>
2020-09-29 02:09:58 -0400

    include Pods

---
+++ Pods/Target Support Files/Pods-TestTvApp-TestTvAppUITests/Pods-TestTvApp-TestTvAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestTvApp_TestTvAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestTvApp_TestTvAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-TestTvApp/Pods-TestTvApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestTvApp : NSObject
+@end
+@implementation PodsDummy_Pods_TestTvApp
+@end
